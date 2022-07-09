ca8de1b7b2f6e372ab867ae9872f8476eb750356
Kevin Tonthat <tonthat.kevin@gmail.com>
2022-07-08 23:42:37 -0700

    Beach Data Component

--- src/components/BeachData.js
+++ src/components/BeachData.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import Card from './UI/Card';
+import classes from './BeachData.module.css';
 
-const BeachData = () => {
-
+const BeachData = (props) => {
+    return (
+        <Card className={classes.beachdatalist}>
+            <ul>
+                {props.beachList.map(beach => <li>{beach.name}, {beach.date} {beach.time}</li>)}
+            </ul>
+        </Card>
+    )
 }
 
 export default BeachData;
